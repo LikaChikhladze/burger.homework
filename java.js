828fe02d821512b973ae576c7d61340c9cc2a15a
LikaChikhladze <chixladze.lika@gmail.com>
2024-01-21 14:19:50 +0300

    burger homework

---
+++ java.js
@@ -0,0 +1,21 @@
+
+let burger = document.querySelector('.burger');
+let wraper = document.querySelector('.burger-wraper');
+let burgerNav = document.querySelector('.burger-nav');
+let spanX = document.querySelector('.spanX');
+let spanX2 = document.querySelector('.spanX2')
+
+burger.addEventListener('click', function() {
+  // wraper.classList.toggle('wraper2');
+  document.body.classList.toggle('newBody');
+  burgerNav.classList.toggle('toggle');
+  spanX.classList.toggle('spanX2');
+  burger.classList.toggle('burger2');
+})
+document.querySelector('.spanS').addEventListener('click', function(){
+  document.body.classList.remove('newBody');
+  spanX.classList.remove('spanX2');
+  burgerNav.classList.remove('toggle');
+  burger.classList.remove('burger2');
+
+})
